cc73ee8e2036345165fdaef77830986af7ee9924
James Trew <j.trew10@gmail.com>
2021-04-03 17:19:37 -0400

    Change user to player.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,11 @@
-import { BrowserRouter, Route } from 'react-router-dom'
-import { User } from './components/User'
+import { BrowserRouter, Route } from "react-router-dom";
+import { Player } from "./components/Player";
 
-const Home = () => (
-    <h1>WElcome to cS ANalYTics</h1>
-)
+const Home = () => <h1>WElcome to cS ANalYTics</h1>;
 
 export const App = () => (
-    <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route path="/user/:xuid" component={User} />
-    </BrowserRouter>
-)
-
+  <BrowserRouter>
+    <Route exact path="/" component={Home} />
+    <Route path="/player/:xuid" component={Player} />
+  </BrowserRouter>
+);
--- react-app/src/components/User.jsx
+++
@@ -1,3 +0,0 @@
-export const User = (props) => {
-  return <h1>Hello {props.match.params.xuid}!</h1>;
-};
