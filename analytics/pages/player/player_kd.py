2cc730312eba95ae48fad0271b8ea1d93f9b37d6
James Trew <j.trew10@gmail.com>
2021-04-10 19:39:43 -0400

    KD stat logic and serializer.

---
+++ analytics/pages/player/player_kd.py
@@ -0,0 +1,20 @@
+from analytics.models import PlayerDeath
+from rest_framework import serializers
+
+
+class KDStat:
+    def __init__(self, xuid: str):
+        self.xuid = xuid
+        self.kills = PlayerDeath.objects.filter(attacker__xuid=self.xuid).count()
+        self.deaths = PlayerDeath.objects.filter(player__xuid=self.xuid).count()
+
+        if self.deaths:
+            self.kd = self.kills / self.deaths
+        else:
+            self.kd = self.kills
+
+
+class KDSerializer(serializers.Serializer):
+    kills = serializers.IntegerField()
+    deaths = serializers.IntegerField()
+    kd = serializers.DecimalField(max_digits=None, decimal_places=2)
